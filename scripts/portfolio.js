659fa4427341410d8d660b08ba21280d553a6a40
geneggy <geneggy@gmail.com>
2021-03-24 00:11:30 -0700

    testing external image hosting

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -56,21 +56,21 @@ const portfolio = (function () {
     
     <div id= 'munite' class = 'hover2 card'>
     <a href="https://geneggy.github.io/munite-pro/" target="_blank">
-      <img src="../munite.png" class="websiteimg" >
+      <img src="https://lh3.googleusercontent.com/Xh-ts6r6qky56JiX82pHLtSRgYqRoked3C5NTMhAyKoXYRnDt7UcIXj55ORwk81WXyfba469kOZOddmmXNasnarMU6ttDBb9zGvXlYcZm5xIkyI6wraGFFPGcrrl-05L92OjuV75tsKRNrgeYcSKFGvJI7PRiQucZgX82huxKMO8X1rKWZY67nKx0XNqMOLzHp6mqpjBHWfbbw6vcfRxNcpWqZnuJeXFmeF8RUgakgkIdxT6j-CS7aK02G1PIZxwzTEbixC7pWuwjfk0b72Wk0T_WhFCVGliRco27VU725eCalC0g1sD4vYYpZYVFOPsJ-tLYKH9pjhdLGzssRkl8FfCU6lpdW-9AQGcWmDnGE-dJTr9Xnl3ZPTm6YCflg5_KA0tFEWBlGktFewShwutbGrfG__19Snz7uROLQjn-vkOKsiwrsiLlfL3yeU2dvv4iXkuh1zuDd6Vtgb-2ARUdX50HXcV7HRokMhFc0PmoqABaBr_vO70cgWpWBjGydt8qEgQ5ldVPXcAeK2M6I8a7ZX8aNq4Lz3kkDDRfgI0I-TEtQW-W1fGbVyGEldJbYuNaaoDQUzybaGqF5sCczZJX2cfReASgfyzuveaLQywqiHqmOkhoGqawzpiHeMahjckCcKxLyinjbgRDaLqQdI-LXJ84OWuGMRJiq_ZQja8FJdqR6oGlbKS_SvNXcXSKFpp2rZZhwXWxn04IderGRBeM8I=w316-h109-no?authuser=0" class="websiteimg" >
       <p class = 'left'>Live demo business website for a meat imports company</p>
       </a>
     </div>
 
     <div id= 'usa' class = 'hover2 card'>
     <a href="https://geneggy.github.io/web_project_3/" target="_blank">
-    <img src="../usa.png" class="websiteimg" >
+    <img src="https://lh3.googleusercontent.com/S5Ry-y0rNAfhJhOVpIqifWZLP17CIm3EQr_DZnXd-K1NVfDHwhOl6u7R3exJ8tKXiymGi10L-OPOlRwrSGVlcPeOQw711xR3g7mGqM0GoMx1ycLleiaxGUuF6e_0KJpW93j7a5oEWcNvnTzdHrW_PxsquQN5u98cDwYbSdqwUAaVXmBwEXn-_NNf1oJZVLRNKuRGxSm95cBle_7NVUwrMhDTcX_uBgZNl9Py2LGPTkejY_ZZVuyetFJx65uwzBE86NXcpbY-JY0EYxnZzWBML1fQJJEF1RbCuNm61W-OwyFUbkVzzPm2VQ1B-TP9eSRAf7ws30vwLeUKSWxV6TwAu9sidpmmIyIiVrsNqjG5EDLxt5ICNbhlOLBqz_oJWoeB4j7aiKXIOkGiRcNlMZjpryLceQkJ1Ng6Sj_hTV1SKZ3sBjzNia2itH9Kx3M_JQhm_DmvFdwGSOJfxgf-Pm32UxLWO1aTulJ_scAwl70qQO647BYDf6BEBelrpjs4u0AEUoGgyGjknf_aNVORY6aZZd_Qu3efluRBs6AiCAMqHNUQKnYranlpY7VEsYzOTEazXmBmeNFX1oMigTLmXCQR4S5WypJHf_9Hspcs-RCIhQhLgU703ewfeXFU79ofAK1ff-vcdbVY-p45XIyaUFP1sGO19oDcDoNFcBvrlVEPck1Aigb61yNuXLoxXzViSXMebwDMAlMVLPf3n2cm8m4wEUw=w329-h116-no?authuser=0" class="websiteimg" >
       <p class = 'left'>Travel blog website aobut a bike journey from Portland, ME to Portland, OR with an image gallery.</p>
       </a>
     </div>
 
     <div id= 'ypracticum' class = 'hover2 card'>
     <a href="https://geneggy.github.io/web_project_1/" target="_blank">
-    <img src="../ypracticum.png" class="websiteimg" >
+    <img src="https://lh3.googleusercontent.com/_rJTDZg5Vl7k83aiezxwD7wFtuI3iP4hKQGSbdGZgR-1hdklOzeEj0tmty-vELh4TUr_zbGHmqiTcQHI6u1uBIbjSFzDJm10AIHSTRkKaYOOxYq8aJq7RTTy23XkwN0pxBd2hph5HCic6Iem6T9CV5ERiikxvjncabLeNCQDiSqpjZMiN3hGs_BYbpLGBqfOrkjBMwh1n3OhzJxFb-tlmCQAl7VPvsH_2rZlyLf6BEcxgbxxg20_DXmxvHyFC87ZekP75jJAJpIbAqSoSj69WHKM9ZLp0Vn6u9ZeSzaclhzuKniGlo2Xi1vkJHv1neBQi1-DcC6GqMaFjEnazDG3qTkRbF_fsgrKglJouCVkAA0oZ-vv9D02I4VILNWO_KPFr7P63R_-RTdSBwGpr-zYNT17uKT88FFq_LApESUe4DfSUSs0kaoJndwNiSal2kiAM6XRKCTbD2LViBZN0gB5bVN_55pTGd-M8SXr4cG8AYAQ5BoYDQBuADS6RhkRU4GtpT3Lp5fKlBwJnWNnazkP2ot5psL5Pgx2vHpwTqko1OvKOkWJOyd3UPzlc--_ZYs2IYACRukctn_yvJ0VtB0uCqSG-5WQa6eKZtrtl6ZChcfbwrt0kCrNAi9ZrLpr1oA8lwiflxEgE2AaVOB3IMMDw808OCi1D-iT0Cytov7LidiZ0juaECPlrmsA3g3diP9q5fl6CsEWA8qgu_J52BS5MSI=w318-h107-no?authuser=0" class="websiteimg" >
       <p class = 'left'>In website that compiles links and resources on how to learn</p>
     </div>
     </a>
